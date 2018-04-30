c6dad2acf0f1d0c98d4753a5375da10f04b8733b
punithkumar <punithkumar325@gmail.com>
2018-04-30 14:59:48 +0530

    second commit

---
+++ Test/src/com/pojo/Article.java
@@ -0,0 +1,22 @@
+package com.pojo;
+
+public class Article {
+	
+	private String articleid;
+	private String articlename;
+	public String getArticleid() {
+		return articleid;
+	}
+	public void setArticleid(String articleid) {
+		this.articleid = articleid;
+	}
+	public String getArticlename() {
+		return articlename;
+	}
+	public void setArticlename(String articlename) {
+		this.articlename = articlename;
+	}
+	
+	
+
+}
